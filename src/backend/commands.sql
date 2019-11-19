5f7cb3e766545b1246fe88aa3c000479984f56b9
Byron <byronhe98@gmail.com>
2019-11-19 14:21:18 -0600

    added in insert statements for every table
    Added in insert statements for every table based on data given to us in the csv files.
    We may want to change the constraints for the `suppliers` table to accept null values since, in the data given to us, there is a row in the suppliers table with null values.

--- src/backend/commands.sql
+++ src/backend/commands.sql
@@ -110,3 +110,114 @@ CONSTRAINT `FK_81` FOREIGN KEY `fkIdx_81` (`book_id`) REFERENCES `book` (`book_i
 KEY `fkIdx_84` (`order_id`),
 CONSTRAINT `FK_84` FOREIGN KEY `fkIdx_84` (`order_id`) REFERENCES `order` (`order_id`)
 );
+
+-- ************** Populating Commands ****************;
+-- ***************************************************;
+
+-- ************************************** `employees`
+INSERT INTO `employees` 
+(
+ `employee_id`, `first_name`, `last_name`, `phone`
+)
+VALUES 
+(1, 'firstname5', 'lastname5'),
+(2, 'firstname6', 'lastname6'),
+(3, 'firstname9', 'lastname6');
+
+-- ************************************** `customers`
+INSERT INTO `customers`
+(
+ `customer_id`, `first_name`, `last_name`, `phone`
+)
+VALUES
+(1, 'firstname1', 'lastname1', '334-001-001'),
+(2, 'firstname2', 'lastname2', '334-002-002'),
+(3, 'firstname3', 'lastname3', '334-003-003'),
+(4, 'firstname4', 'lastname4', '334-004-004');
+
+-- ************************************** `suppliers`
+INSERT INTO `suppliers`
+(
+ `supplier_id`, `company_name`, `contact_last_name`, `contact_first_name`, `phone`
+)
+VALUES
+(1, 'supplier1', 'company1', 'company1', '1111111111'),
+(2, 'supplier2', 'company2', 'company2', '2222222222'),
+(3, 'supplier3', 'company3', 'company3', '3333333333');
+-- ************************************** adding in a row with NULL attributes
+INSERT INTO `suppliers`
+(
+ `supplier_id`, `company_name`, `contact_last_name`, `phone`
+)
+VALUES
+(4, 'supplier4', 'company4', '4444444444');
+
+-- ************************************** `shippers`
+INSERT INTO `shippers`
+(
+ `shipper_id`, `name`
+)
+VALUES
+(1, 'shipper1'),
+(2, 'shipper2'),
+(3, 'shipper3'),
+(4, 'shipper4');
+
+-- ************************************** `subjects`
+INSERT INTO `subjects`
+(
+ `subject_id`, `category`
+)
+VALUES
+(1, 'category1'),
+(2, 'category2'),
+(3, 'category3'),
+(4, 'category4'),
+(5, 'category5');
+
+-- ************************************** `orders`
+INSERT INTO `orders`
+(
+ `order_id`, `customer_id`, `employee_id`, `date_ordered`, `date_shipped`, `shipper_id`
+)
+VALUES
+(1, 1, 1, '8/1/2016', '8/3/2016', 1),
+(2, 1, 2, '8/4/2016', NULL, NULL),
+(3, 2, 1, '8/1/2016', '8/4/2016', 2),
+(4, 4, 2, '8/4/2016', '8/4/2016', 1),
+(5, 1, 1, '8/4/2016', '8/5/2016', 1),
+(6, 4, 2, '8/4/2016', '8/5/2016', 1),
+(7, 3, 1, '8/4/2016', '8/4/2016', 1);
+
+-- ************************************** `books`
+INSERT INTO `books`
+(
+ `book_id`, `title`, `unit_price`, `author`, `quantity`, `supplier_id`, `subject_id`
+)
+VALUES
+(1, 'book1', 12.34, 'author1', 5, 3, 1),
+(2, 'book2', 56.78, 'author2', 2, 3, 1),
+(3, 'book3', 90.12, 'author3', 10, 2, 1),
+(4, 'book4', 34.56, 'author4', 12, 3, 2),
+(5, 'book5', 78.90, 'author5', 5, 2, 2),
+(6, 'book6', 12.34, 'author6', 30, 1, 3),
+(7, 'book7', 56.90, 'author2', 17, 3, 4),
+(8, 'book8', 33.44, 'author7', 2, 1, 3);
+
+-- ************************************** `order_details`
+INSERT INTO `order_details`
+(
+ `book_id`, `order_id`, `quantity`
+)
+VALUES
+(1, 1, 2),
+(4, 1, 1),
+(6, 2, 2),
+(7, 2, 3),
+(5, 3, 1),
+(3, 4, 2),
+(4, 4, 1),
+(7, 4, 1),
+(1, 5, 1),
+(1, 6, 2),
+(1, 7, 1);
